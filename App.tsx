dd0d0c415624d566eaf84a6b0a1c6f6ec206fea2
princewhyte2 <princewhyte02@gmail.com>
2021-06-09 02:29:08 +0100

    feat: Tipcalculator app implementation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  Provider as PaperProvider,
+  TextInput,
+  RadioButton,
+  Text,
+  Button,
+} from "react-native-paper";
+import { StyleSheet, View, Switch } from "react-native";
+import currencyFormatter from "currency-formatter";
 
 export default function App() {
+  const [cost, setCost] = React.useState("");
+  const [value, setValue] = React.useState(() => "Amazing");
+  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
+  const [displayTip, setDisplayTip] = React.useState("");
+
+  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
+  const calculateTip = () => {
+    let costFloat = parseFloat(cost);
+    if (costFloat == NaN) {
+      return;
+    }
+    let tipPercentage;
+    switch (value) {
+      case "Amazing":
+        tipPercentage = 0.2;
+        break;
+      case "Good":
+        tipPercentage = 0.18;
+        break;
+      default:
+        tipPercentage = 0.15;
+        break;
+    }
+    let tip = tipPercentage * costFloat;
+
+    if (isSwitchOn) {
+      tip = Math.ceil(tip);
+    }
+    let val = currencyFormatter.format(tip, { code: "NGN" });
+
+    setDisplayTip(val);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <TextInput
+          keyboardType="numeric"
+          value={cost}
+          onChangeText={(cost) => setCost(cost)}
+          placeholder="Cost of Service"
+        />
+        <Text>How was the service?</Text>
+        <RadioButton.Group
+          onValueChange={(newValue) => setValue(newValue)}
+          value={value}
+        >
+          <View style={{ width: 200 }}>
+            <RadioButton.Item
+              position="leading"
+              label="Amazing (20%)"
+              value="Amazing"
+            />
+          </View>
+          <View style={{ width: 175 }}>
+            <RadioButton.Item
+              position="leading"
+              label="Good (18%)"
+              value="Good"
+            />
+          </View>
+          <View style={{ width: 175 }}>
+            <RadioButton.Item
+              position="leading"
+              label="Okay (15%)"
+              value="Okay"
+            />
+          </View>
+        </RadioButton.Group>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text>Round up tip?</Text>
+          <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
+        </View>
+        <Button mode="contained" onPress={calculateTip}>
+          Calculate
+        </Button>
+
+        <View style={{ alignItems: "flex-end" }}>
+          <Text>Tip Amount:{displayTip}</Text>
+        </View>
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    marginTop: 66,
+    marginLeft: 16,
+    marginRight: 16,
   },
 });
